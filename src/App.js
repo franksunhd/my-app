58cf61494e61084e57d13a8cafad052992884d57
franksun <sun_si_yan@163.com>
2020-01-10 14:13:06 +0800

    修改架构

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 // 引入React
 import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import {BrowserRouter, HashRouter} from "react-router-dom";
 // 引入全局的css
-import './App.css';
-import AllDom from './fix';
+import './assets/css/index.css';
+// eslint-disable-next-line no-unused-vars
+import AllDom from './page/fix';
 
+// 直接挂载到域名根目录
+export const ROUTE_BASE_NAME = process.env.BASE_NAME || '';
 
 // 根节点渲染函数
 function App() {
     return (
-        <div className="App">
-            <AllDom/>
-        </div>
+        <BrowserRouter basename={ROUTE_BASE_NAME}>
+            <div style={{display: 'flex', flexDirection: 'column', position: 'relative', minHeight: '100vh'}}>
+
+            </div>
+        </BrowserRouter>
     );
 }
 
