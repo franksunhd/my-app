ad64bbb1c820d54d42ec11437873fe947d777070
franksun <sun_si_yan@163.com>
2020-01-12 17:43:50 +0800

    动态判断水是否沸腾了

--- src/page/pages/Basic/core_2.jsx
+++ src/page/pages/Basic/core_2.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 // 引入标题组件
 import TitleText from "../common/titleText";
 
-// 状态提升
+// 1. 状态提升
 class StatusUp extends Component {
     // 构造函数
     // eslint-disable-next-line no-useless-constructor
@@ -13,7 +13,44 @@ class StatusUp extends Component {
     render() {
         return (
             <div className="statusUp">
-                <TitleText title="状态提升"/>
+                <TitleText title="1. 状态提升"/>
+                <BoilingVerdict celsius={1}/>
+            </div>
+        )
+    }
+}
+
+// 判断水是否沸腾
+function BoilingVerdict(props) {
+    if (props.celsius >= 100) {
+        return <h1>温度{props.celsius}° ,水沸腾了</h1>
+    } else {
+        return <h1>温度{props.celsius}°, 水没有沸腾</h1>
+    }
+}
+
+// 2. 动态判断水是否判断
+class Calculator extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {temperature: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({temperature: e.target.value});
+    }
+
+    render() {
+        let temperature = this.state.temperature;
+        return (
+            <div>
+                <TitleText title="2. 动态判断水是否判断"/>
+                <fieldset>
+                    <legend>Enter temperature in Celsius:</legend>
+                    <input value={temperature} onChange={this.handleChange}/>
+                    <BoilingVerdict celsius={temperature !== '' ? parseFloat(temperature) : 0}/>
+                </fieldset>
             </div>
         )
     }
@@ -24,7 +61,11 @@ class StatusUp extends Component {
 function Core_2() {
     return (
         <div className="Core_2_Box padding20">
+            {/* 1. 状态提升 */}
             <StatusUp/>
+
+            {/* 2. 动态判断水是否沸腾 */}
+            <Calculator/>
         </div>
     )
 }
