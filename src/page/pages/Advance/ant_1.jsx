50873df5bd7773cd143559cf9a14138c91696294
franksun <sun_si_yan@163.com>
2020-01-19 14:39:32 +0800

    布局

--- src/page/pages/Advance/ant_1.jsx
+++ src/page/pages/Advance/ant_1.jsx
@@ -326,6 +326,7 @@ class LayoutBox extends Component {
     }
 }
 
+// 6. 菜单布局
 class SiderDemo extends Component {
     constructor(props) {
         super(props);
@@ -345,40 +346,43 @@ class SiderDemo extends Component {
 
         const {Header, Sider, Content} = Layout;
         return (
-            <Layout>
-                <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                    <div className="logo"/>
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Icon type="user"/>
-                            <span>nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera"/>
-                            <span>nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload"/>
-                            <span>nav 3</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
+            <div>
+                <TitleText title="6. 菜单布局"/>
                 <Layout>
-                    <Header style={{background: '#fff', padding: 0}}>
-                        <Icon className="trigger"
-                              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                              onClick={this.toggle}/>
-                    </Header>
-                    <Content style={{
-                        margin: '24px 16px',
-                        padding: 24,
-                        background: '#fff',
-                        minHeight: 280,
-                    }}>
-                        Content
-                    </Content>
+                    <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                        <div className="logo"/>
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">
+                                <Icon type="user"/>
+                                <span>nav 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="video-camera"/>
+                                <span>nav 2</span>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Icon type="upload"/>
+                                <span>nav 3</span>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{background: '#fff', padding: 0}}>
+                            <Icon className="trigger"
+                                  type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                  onClick={this.toggle}/>
+                        </Header>
+                        <Content style={{
+                            margin: '24px 16px',
+                            padding: 24,
+                            background: '#fff',
+                            minHeight: 280,
+                        }}>
+                            Content
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
+            </div>
         );
     }
 }
@@ -401,7 +405,7 @@ function ANT_1() {
             {/* 5. 布局 */}
             <LayoutBox/>
 
-            {/* 6. */}
+            {/* 6. 菜单布局 */}
             <SiderDemo/>
         </div>
     )
